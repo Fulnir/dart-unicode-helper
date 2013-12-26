6a03f1a18bc7801a9cf62a5bd0b295a381b1e8e3
Edwin Bühler <edbuehler@icloud.com>
2013-12-26 11:28:28 +0100

    Add changelog file.

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# Changelog - Dart Unicode Helper
+
+## 0.1.2 2013-12-26
+
+* Add this changelog.md file.
+* Add hop runner file.
+* Add dgh-pages.
---
+++ tool/hop_runner.dart
@@ -0,0 +1,15 @@
+// $ cd /Users/ed/Documents/Developing/dart/RangedSparseList
+// $ dart tool/hop_runner.dart --color docs --allow-dirty
+//
+library sparse_list.hop_runner;
+
+
+import 'package:hop/hop.dart';
+import 'package:hop/hop_tasks.dart';
+
+void main(List<String> args) {
+
+  addTask('docs', createDartDocTask(['lib/unicode_helper.dart']));
+
+  runHop(args, paranoid: false);
+}
